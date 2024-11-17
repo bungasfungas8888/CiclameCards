18b4eedce10327971fddff43c465b335efd64862
Soraoh <00001135665825sp@al.educacao.sp.gov.br>
2024-11-16 22:52:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Vagabond',
+    'Quem é o criador de Vagabond',
+    'Takehiko Inoue'
+)
+
+criaCartao(
+    'Vagabond',
+    'Qual o nome do aprendiz de Musashi',
+    'Joutaro'
+)
+
+criaCartao(
+    'Vinland Saga',
+    'Qual o nome da irmã de Thorfinn',
+    'Ylva'
+)
+
+criaCartao(
+    'DanDaDan',
+    'Qual o personagem mais poderoso da obra',
+    'Ludris'
+)
